0e7c230728a6b5d3aa9488e2d2aedbc1746d2a47
Daniel Farnan <danielfarnan1@gmail.com>
2022-12-20 22:56:29 -0800

    -m "initial  upload"

---
+++ 10.2_es2015_let_and_const.js
@@ -0,0 +1,18 @@
+//What is the difference between var and let?
+    A Var declared variable is hoisted to the top of the document and can technically be called 
+    before it has been declared a Let declared variable can only be called once it has been declared.
+
+//What is the difference between var and const?
+    A Var declared variable can be reassigned to any other value but a Const declared variable is 
+    fixed to the value it is giving.
+
+//What is the difference between let and const?
+    A let declared variable can be modified i.e. added to or changed after it has been assigned but a 
+    const variable can not be changed in any way
+
+//What is hoisting?
+    Hoisting is the process of variables being declared at the beginning of the scope 
+
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+    const PI = 3.14;
